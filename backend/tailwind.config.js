75926083b90349b79e4b05438c04ae31bc1d0e74
Justin Leger <juslleg@gmail.com>
2021-08-29 15:30:41 -0400

    ✨ Add tailwind css
    you can use `bin/tailwind` to listen to changes and regenerate the style.css

---
+++ backend/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  purge: [
+    './views/**/*.erb',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
